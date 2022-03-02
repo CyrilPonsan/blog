7c1b4ce3eb2780cc452a26978a605e5cef9293df
CyrilPonsan <cyril.ponsan@gmail.com>
2022-03-02 14:44:16 +0100

    categorie article ok

---
+++ src/Entity/Article.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $titre;
+
+    #[ORM\Column(type: 'text')]
+    private $contenu;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $imageSrc;
+
+    #[ORM\Column(type: 'integer')]
+    private $nbreVues;
+
+    #[ORM\ManyToOne(targetEntity: Categorie::class, inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getImageSrc(): ?string
+    {
+        return $this->imageSrc;
+    }
+
+    public function setImageSrc(?string $imageSrc): self
+    {
+        $this->imageSrc = $imageSrc;
+
+        return $this;
+    }
+
+    public function getNbreVues(): ?int
+    {
+        return $this->nbreVues;
+    }
+
+    public function setNbreVues(int $nbreVues): self
+    {
+        $this->nbreVues = $nbreVues;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
