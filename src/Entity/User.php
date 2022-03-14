d72c2f736bf916c6f73d0e65ff61b969ee3fef47
CyrilPonsan <cyril.ponsan@gmail.com>
2022-03-14 15:15:37 +0100

    date

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -212,7 +212,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __toString(): string
     {
-        return $this->pseudo;
+        return $this->username;
     }
 
     public function isVerified(): bool
